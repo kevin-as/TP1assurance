9e3a9556c9892c581c0c56878930f526bcc29d26
kevin abouserhal <kevinabouserhal@hotmail.com>
2020-02-03 13:32:10 -0500

    Création du constructeur

--- src/CV.java
+++ src/CV.java
@@ -1,4 +1,22 @@
 
 public class CV {
 
+	String nom;
+	String prenom;
+	String formation;
+	int experienceTravail;
+	String[] competences;
+	String attentes;
+	
+	public CV(String pNom, String pPrenom, String pFormation, int pExperienceTravail, 
+			String[] pCompetences, String pAttentes) {
+		
+		nom = pNom;
+		prenom = pPrenom;
+		formation = pFormation;
+		experienceTravail = pExperienceTravail;
+		competences = pCompetences;
+		attentes = pAttentes;
+		
+	}
 }
