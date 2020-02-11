469c57096ad938e20f21248e2dba5c6fe95510ad
kevin abouserhal <kevinabouserhal@hotmail.com>
2020-02-11 11:21:26 -0500

    Création de la classe FichierSortie et de la méthode sauvegarderFichier

---
+++ src/FichierSortie.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class FichierSortie {
+	
+	public static void sauvegarderFichier( String[] factures ) throws IOException {
+		
+		String nomFichier = "fichierSortie.txt";
+		BufferedWriter writer = new BufferedWriter(new FileWriter(nomFichier));
+		
+		writer.write("Bienvenue chez Barette!\n");
+		writer.write("Factures:\n");
+		
+		for (int i = 0; i < factures.length; i++) {
+			writer.write(factures[i] + "\n");
+		}
+		
+		writer.close();
+	}
+	
+	
+}
