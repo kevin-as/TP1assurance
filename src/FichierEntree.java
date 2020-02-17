0e61da479670b3003252d48aab5f9ddc04c05124
Charles-Olivier Allard <artemis2713@hotmail.com>
2020-02-17 09:09:11 -0500

    Méthode separerLignesFichier nom complète

--- src/FichierEntree.java
+++ src/FichierEntree.java
@@ -5,9 +5,9 @@ public class FichierEntree {
 	
 	public static String[] clients;
 	public static String[][] plats;
-	public static String [][][] commandes;
+	public static String [][] commandes;
 	
-	public static String lireNomFichier() {
+	private static String lireNomFichier() {
 		Scanner entreeUtil = new Scanner(System.in);
 		
 		System.out.print("\nEntrez le nom du fichier à importer : ");
@@ -18,11 +18,12 @@ public class FichierEntree {
 		return nomFichier;
 	}
 	
-	public static String[] lireFichier() throws IOException{
+	private static String[] lireFichier() throws IOException{
 		String nomFichier = lireNomFichier();
 		Scanner scanner;
 		List<String> listeLignesFichier = new ArrayList<>();
 		String[] lignesFichier = null;
+		
 		try {
 			scanner = new Scanner(new File(nomFichier));
 			while (scanner.hasNextLine()) {
@@ -38,6 +39,50 @@ public class FichierEntree {
 		return lignesFichier;
 	}
 	
-	
+	public static void separerLignesFichier() throws IOException{
+		String[] lignesFichier = lireFichier();
+		
+		int noLigne = 1;
+		List<String> listeClients = new ArrayList<>();
+		List<String> listePlats = new ArrayList<>();
+		List<String> listeCommandes = new ArrayList<>();
+		
+		noLigne = separerParType(lignesFichier, noLigne, listeClients);
+		clients = listeClients.toArray(new String[0]);
+		
+		noLigne = separerParType(lignesFichier, noLigne, listePlats);
+		
+		
+		while(!lignesFichier[noLigne].equals("Fin")) {
+			listeCommandes.add(lignesFichier[noLigne]);
+			noLigne++;
+		}
+		
+		System.out.println("CLIENTS");
+		for(String s : clients) {
+			System.out.println(s + ", ");
+		}
+		/*
+		System.out.println("PLATS");
+		for(String s : listePlats) {
+			System.out.println(s + ", ");
+		}
+		
+		System.out.println("COMMANDES");
+		for(String s : listeCommandes) {
+			System.out.println(s + ", ");
+		}
+		*/
+		
+	}
+
+	private static int separerParType(String[] lignesFichier, int noLigne, List<String> listeVoulue) {
+		while(!lignesFichier[noLigne].contains(":")) {
+			listeVoulue.add(lignesFichier[noLigne]);
+			noLigne++;
+		}
+		noLigne++;
+		return noLigne;
+	}
 	
 }
--- src/Principal.java
+++ src/Principal.java
@@ -3,9 +3,10 @@ import java.io.*;
 public class Principal {
 
 	public static void main(String[] args) throws IOException {
-		String[] factures = new String[]{"Roger 10.50$","Céline 20.75$","Steeve 0.00$"};
-		FichierSortie.sauvegarderFichier( factures );
+		//String[] factures = new String[]{"Roger 10.50$","Céline 20.75$","Steeve 0.00$"};
+		//FichierSortie.sauvegarderFichier( factures );
 		
+		FichierEntree.separerLignesFichier();
 	}
 
 }
