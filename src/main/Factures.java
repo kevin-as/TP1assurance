c585467e13ed9b24df0643041fe4c2a9ae127783
Charles-Olivier Allard <artemis2713@hotmail.com>
2020-03-10 10:42:32 -0400

    testTaxes fonctionnel

--- src/main/Factures.java
+++ src/main/Factures.java
@@ -6,18 +6,30 @@ public class Factures {
 	
 	static final double TAUX_TPS = 0.05;
 	static final double TAUX_TVQ = 0.09975;
-	private static final DecimalFormat DF = new DecimalFormat("0.00");
+	private static final DecimalFormat TOTAL = new DecimalFormat("0.00");
+	private static final DecimalFormat PRE_TOTAL = new DecimalFormat("0.000");
 	
 	public static double calculerTaxes(double sousTotal) {
 		double total, montantTPS, montantTVQ;
+		String totalChaine;
 		
 		montantTPS = sousTotal * TAUX_TPS;
 		montantTVQ = sousTotal * TAUX_TVQ;
 		
 		total = sousTotal + montantTPS + montantTVQ;
 		
-		total = Double.valueOf(DF.format(total));
+		total = Double.valueOf(PRE_TOTAL.format(total));
+		
+		totalChaine = String.valueOf(total);
+		
+		if(totalChaine.charAt(totalChaine.length()-1) == '5') {
+			total = Double.valueOf(TOTAL.format(total));
+			total += 0.01;
+		}else {
+			total = Double.valueOf(TOTAL.format(total));
+		}
 		
 		return total;
 	}
+	
 }
--- src/test/TestFactures.java
+++ src/test/TestFactures.java
@@ -11,6 +11,8 @@ public class TestFactures {
 	@Test
 	public void testTaxes() {
 		assertEquals(114.98, Factures.calculerTaxes(100), 0);
+		assertEquals(63.24, Factures.calculerTaxes(55), 0);
+		assertEquals(27.02, Factures.calculerTaxes(23.50), 0);
 	}
 
 }
