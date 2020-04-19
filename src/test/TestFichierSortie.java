b8ee66fd809b87f0c5f7874cc7ef7bb201379b75
Charles-Olivier Allard <artemis2713@hotmail.com>
2020-04-19 13:43:58 -0400

    Coverage à 70.9%

--- src/test/TestFichierSortie.java
+++ src/test/TestFichierSortie.java
@@ -3,6 +3,8 @@ package test;
 import static org.junit.Assert.*;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.junit.Test;
 
@@ -11,11 +13,14 @@ import main.FichierSortie;
 public class TestFichierSortie {
 	
 	String[][] factures = { {"Roger", "Poutine", "2"}, {"Céline", "Frites", "2"}, {"Steeve", "Repas_Poulet", "1"} };
+	SimpleDateFormat formatDateHeure = new SimpleDateFormat("dd-MM-HH-mm");
+	Date date = new Date();
+	String nomFichier = "Facture-du-" + formatDateHeure.format(date) + ".txt";
 	
 	@Test
 	public void testSauvegarderFichierString() throws IOException {
 		//actualiser date et heure avant de tester à nouveau
-		assertEquals("","Facture-du-18-04-20-11.txt", FichierSortie.sauvegarderFichierString("yes"));
+		assertEquals("", nomFichier, FichierSortie.sauvegarderFichierString("yes"));
 	}
 	
 	@Test
